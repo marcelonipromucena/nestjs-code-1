9a1e986a3b7ac7597e7b6619685c5c5509eaa0b6
Marceloni Promucena <marcelonipromucena@gmail.com>
2020-09-12 14:52:37 -0300

    aula 42

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
@@ -23,14 +23,19 @@ export class TasksService {
       tasks = tasks.filter(
         task =>
           task.title.includes(search) || task.description.includes(search),
-       );
+      );
     }
 
     return tasks;
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const task = this.tasks.find(task => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -48,7 +53,9 @@ export class TasksService {
   }
 
   deleteTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const task = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter(task => task.id !== task.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
